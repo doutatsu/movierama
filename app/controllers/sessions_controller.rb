bac58c507388984c52e76022ba7970069c959370
doutatsu <doutatsu@me.com>
2016-07-04 23:17:17 +0100

    [Feature] Notify user if his email is private

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
   def create
     UserRegistration.new(auth_hash).tap do |ur|
-      authenticate! ur.user
+      authenticate! ur.user       
 
       if ur.created?
         flash[:notice] = "Account created"
+        flash[:alert] = "Your github email is private. You won't be able to get email notifications." if auth_hash["info"]["email"].nil?
       else
         flash[:notice] = "Welcome back"
+        flash[:alert] = "Your github email is private. You won't be able to get email notifications." if auth_hash["info"]["email"].nil?
       end
     end
     redirect_to root_url
